e50fa9dbc6d3b27be5fabd9c7665efeb382f4fbb
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-26 14:31:49 +0100

    two numbers being the same

---
+++ selection exercise 1.py
@@ -0,0 +1,10 @@
+#Lewis Travers
+#26/09/2014
+#Selection exercise 1
+
+number1 = int(input("Please enter your first number: "))
+number2 = int(input("Please enter your second number: "))
+
+if number1 == number2:
+    print("Those numbers are the same")
+    
