e3bf27ca7bb0f003a748e42e73b47f3343e609ae
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-30 12:42:05 +0100

    number between 21 and 29

---
+++ revision exercise 3.py
@@ -0,0 +1,10 @@
+#Lewis Travers
+#30/09/2014
+#Revision exercise 3
+
+number = int(input("Please enter a number: "))
+
+if 29 > number > 21:
+    print("That number is within the range")
+else:
+    print("That number is not within the accepted range.")
