04b52dd6143a8f9e6ef3b29a993af3ed894ea25a
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-09-30 12:20:10 +0100

    Whether a person is old enough to drive

---
+++ revision exercise 1.py
@@ -0,0 +1,9 @@
+#Lewis Travers
+#30/09/2014
+#Revision exercise 1
+
+age = int(input("Please ener your age: "))
+if age >= 17:
+    print("You are old enough to drive.")
+else:
+    print("You are not old enough to drive.")
