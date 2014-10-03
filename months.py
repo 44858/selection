084ec9ad8c58b278e85cd71294a4dcd43369b1fb
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-10-03 13:40:07 +0100

    Entering a number and displaying the associated month

---
+++ months.py
@@ -0,0 +1,32 @@
+#Lewis Travers
+#03/10/2014
+#Months
+
+month = int(input("Please enter a number between 1 and 12:"))
+
+if month == 1:
+    print("You have selected January.")
+elif month == 2:
+    print("You have selected February.")
+elif month == 3:
+    print("You have selected March.")
+elif month == 4:
+    print("You have selected April.")
+elif month == 5:
+    print("You have selected May.")
+elif month == 6:
+    print("You have selected June.")
+elif month == 7:
+    print("You have selected July.")
+elif month == 8:
+    print("You have selected August.")
+elif month == 9:
+    print("You have selected September.")
+elif month == 10:
+    print("You have selected October.")
+elif month == 11:
+    print("You have selected November.")
+elif month == 12:
+    print("You have selected December.")
+else:
+    print("That is not a number within the specified range.")
