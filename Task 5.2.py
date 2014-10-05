f0ebe2a8fd0df97b128d740a5c9e33ca7eb3210a
44858 <l.travers55@gmail.com>
2014-10-05 12:03:21 +0100

    number between the range 1 and 20

---
+++ Task 5.2.py
@@ -0,0 +1,14 @@
+#Lewis Travers
+#25/09/2014
+#Task 5.2
+
+number1 = int(input("Please enter a number in between 1 and 20:"))
+
+if number1 >20:
+    print("That number is too large.")
+elif number1 <1:
+    print("That number is too small.")
+elif number1 <20 and number1 >1:
+    print("That number is within the correct range.")
+
+    
