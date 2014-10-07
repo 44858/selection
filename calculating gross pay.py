3f1000345d6ad2ed8c73ae5cad7829b5268f9aff
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-10-07 12:04:00 +0100

    Calculating gross pay (completed)

--- calculating gross pay.py
+++ calculating gross pay.py
@@ -4,7 +4,7 @@
 
 hours = int(input("Please enter the number of hours worked this week: "))
 
-hourly_pay = int(input("Please enter yuor hourly pay: "))
+hourly_pay = int(input("Please enter your hourly pay: "))
 
 overtime_pay = hourly_pay * 1.5
 
@@ -12,6 +12,8 @@ total_pay = hours * overtime_pay
 
 gross_pay = hours * hourly_pay
 
+if hours >60 or hours <0:
+    print("That is not a valid amount of hours.")
 if hours >= 40:
     print("Your total pay is £{0}".format(total_pay))
 else:
